9763635a789f0782f66f7fd13b880378b9bef377
Anna <anyapryanikova@gmail.com>
2023-08-28 11:34:25 +0300

    count fix

--- app/models/BooksModel.php
+++ app/models/BooksModel.php
@@ -11,10 +11,6 @@ class BooksModel
 {
     public function getDataBooks($limit, $offset, $searchType = null, $searchBook = null)
     {
-        echo $offset;
-        // var_dump($searchBook);
-        // echo PHP_EOL;
-        // var_dump($searchType);
         $db = ConnectDB::getInstance();
 
         if ($searchType === null || ($searchType !== null && $searchBook === '')) {
@@ -32,9 +28,6 @@ class BooksModel
         if ($stmt->execute()!=false) {
             $result = $stmt->get_result();
             $dataBooksArray = $result->fetch_all(MYSQLI_ASSOC);
-    // echo "exec";
-    var_dump(($offset));
-    // var_dump($dataBooksArray);
 
             return $dataBooksArray;
         }
@@ -50,9 +43,9 @@ class BooksModel
             $query = (file_get_contents(__DIR__ . '/../../db/count_rows_books_table.sql'));
             $result = $db->query($query);
         } elseif ($isBooksBySearch === true) {
-            $query = (file_get_contents(__DIR__ . "/../../db/count_books_by_" . urlencode($_GET['select-by']) . "_search.sql"));
+            $query = (file_get_contents(__DIR__ . "/../../db/count_books_by_" . $_GET['select-by'] . "_search.sql"));
             $result = $db->prepare($query);
-            $searchBook = urlencode($_GET['search-book']);
+            $searchBook = $_GET['search-book'];
             
             $result->bind_param("s", $searchBook);
             $result->execute();
@@ -61,7 +54,7 @@ class BooksModel
 
         if (isset($result) && $result && $result->num_rows === 1) {
             $row = $result->fetch_row();
-            echo (int)$row[0];
+            // echo (int)$row[0];
             return (int)$row[0];
         }
 
